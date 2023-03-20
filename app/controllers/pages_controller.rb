bf58ed3632db28c984d8025cfcbf9bebd8306210
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-03-20 18:11:48 +0100

    home page structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @start_link = user_signed_in? ? dashboard_path : new_user_session_path
   end
 end
