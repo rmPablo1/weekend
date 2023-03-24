150c30f65fe8ad8cbccda3cb8e17b27a7ebdbb52
tom burns <tomburns.gj@gmail.com>
2023-03-24 07:42:22 +0000

    fixed all dashboard front end exclusing calendar events

--- app/javascript/controllers/navbar_slider_controller.js
+++ app/javascript/controllers/navbar_slider_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar-slider"
 export default class extends Controller {
-  static targets = ['mainbody', 'navbar']
+  static targets = ['mainbody', 'navbar', 'close']
   connect() {
     console.log('navbar_slider is connected!')
   }
@@ -10,10 +10,12 @@ export default class extends Controller {
   open() {
     this.navbarTarget.classList.remove('navbar-clicked-navbar')
     this.mainbodyTarget.classList.remove('navbar-clicked-mainbody')
+    this.closeTarget.classList.add('hidden')
   }
 
   close() {
     this.navbarTarget.classList.add('navbar-clicked-navbar')
     this.mainbodyTarget.classList.add('navbar-clicked-mainbody')
+    this.closeTarget.classList.remove('hidden')
   }
 }
