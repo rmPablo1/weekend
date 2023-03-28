2f732a3db52381a3b19f8ca892e56462a10f7c5c
rmPablo1 <pablorubiiimonzo@gmail.com>
2023-03-28 10:35:34 +0200

    fixed some bugs

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -6,20 +6,16 @@ class FriendsController < ApplicationController
 
   def show
     @friend_requester = User.find(params[:id])
-    unless @friend_requester.mutual_following_with?(current_user)
       current_user.accept_follow_request_of(@friend_requester)
       current_user.send_follow_request_to(@friend_requester)
       @friend_requester.accept_follow_request_of(current_user)
-    end
-
     redirect_to friends_path
   end
 
   def create
     @user = User.find_by(email:params[:email])
-    unless @user.mutual_following_with?(current_user)
+    unless current_user.mutual_following_with?(@user)
       current_user.send_follow_request_to(@user)
-      @user.send_follow_request_to(current_user)
     end
   end
 
