0aaf6dc0be29e60e9fcd1ecf6211cb5bf61f5ca9
Astrid Chazaux <astridchazaux@hotmail.fr>
2023-03-29 13:03:39 +0200

    homepage corrections fe

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,11 +9,13 @@ class EventsController < ApplicationController
     if event.save!
       params[:event][:user_id].each do |user_id|
         next if user_id == ""
-
-        invitation = Invitation.new
-        invitation.event = event
-        invitation.user = User.find(user_id)
-        invitation.save!
+        event = Event.new(set_params)
+        event.user = User.find(user_id)
+        event.save!
+        # invitation = Invitation.new
+        # invitation.event = event
+        # invitation.user = User.find(user_id)
+        # invitation.save!
       end
       redirect_to dashboard_path
     else
