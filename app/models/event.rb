559f0dfe3aacb6f2a28e348d2ee2559338a6f968
rmPablo1 <pablorubiiimonzo@gmail.com>
2023-03-30 14:11:41 +0200

    fixed some bugs in production and refactored the code a little bit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :invitations
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_142909) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_100756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,15 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_142909) do
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-  create_table "invitations", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_invitations_on_event_id"
-    t.index ["user_id"], name: "index_invitations_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -93,8 +84,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_142909) do
   add_foreign_key "contacts", "users", column: "sender_id"
   add_foreign_key "events", "users"
   add_foreign_key "groups", "users"
-  add_foreign_key "invitations", "events"
-  add_foreign_key "invitations", "users"
   add_foreign_key "users_groups", "groups"
   add_foreign_key "users_groups", "users"
 end
